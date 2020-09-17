bf5336efb38c22378f684b46eb4f17f39137fe2f
ezebloj <ezebloj@gmail.com>
2020-09-17 18:10:43 -0300

    se hizo un deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDu1mpsaf8J3XmbuMJDPomUspwwgRDnqpU',
+    authDomain: 'fir-6de38.firebaseapp.com',
+    databaseURL: 'https://fir-6de38.firebaseio.com',
+    projectId: 'fir-6de38',
+    storageBucket: 'fir-6de38.appspot.com',
+    messagingSenderId: '287950923622',
+    appId: '1:287950923622:web:c22f2807b71228b47bcaa7',
+  },
 };
